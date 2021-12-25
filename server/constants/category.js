2cfbeb9743540e59392759008128ab3eea653077
Parshuram Nikam <parshunikam0612@gmail.com>
2021-12-25 19:40:43 +0530

    stable version - client & server

---
+++ server/constants/category.js
@@ -0,0 +1,16 @@
+const categories = [
+	"electronics",
+	"jewelery",
+	"mensClothing",
+	"womensClothing",
+	"babyAndZKids",
+	"homeAndFurniture",
+	"books",
+	"grocery",
+	"healthAndPersonalCare",
+	"luggageAndBags",
+	"SportsFitnessOutdoors",
+	"Beauty"
+];
+
+export default categories;
